8c2e6f807f2a9aeaf1791aa8f2ec476097d2b552
zhouByte-hub <zhou_jia_ning@163.com>
2026-03-22 14:21:12 +0800

    初始化项目目录

---
+++ camunda_api/src/main/java/org/zhoubyte/camunda_api/CamundaApiApplication.java
@@ -0,0 +1,13 @@
+package org.zhoubyte.camunda_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaApiApplication.class, args);
+    }
+
+}
