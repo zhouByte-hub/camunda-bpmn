ab5c790eed9a455fdb2160dbae7e119d7eb07539
zhouByte-hub <zhou_jia_ning@163.com>
2026-03-25 00:17:25 +0800

    添加发布 API

--- camunda_api/src/main/java/org/zhoubyte/camunda_api/opt/DeployController.java
+++ camunda_api/src/main/java/org/zhoubyte/camunda_api/opt/DeployController.java
@@ -1,10 +1,9 @@
 package org.zhoubyte.camunda_api.opt;
 
 import io.camunda.client.CamundaClient;
+import io.camunda.client.api.response.DeleteResourceResponse;
 import io.camunda.client.api.response.DeploymentEvent;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/deploy")
@@ -16,10 +15,27 @@ public class DeployController {
         this.camundaClient = camundaClient;
     }
 
-    @GetMapping(value = "/resource")
-    public DeploymentEvent deploy(){
+    @GetMapping(value = "/bpmn_resource")
+    public DeploymentEvent deployBpmnResource() {
         return camundaClient.newDeployResourceCommand()
-                .addResourceFile("camunda.yml")
+                .addResourceFromClasspath("bpmn/purchase_server_process.bpmn")
+                .send()
+                .join();
+    }
+
+
+    @GetMapping(value = "/form_resource")
+    public DeploymentEvent deployFormResource() {
+        return camundaClient.newDeployResourceCommand()
+                .addResourceFromClasspath("bpmn/apply_form.form")
+                .send()
+                .join();
+    }
+
+
+    @DeleteMapping(value = "/{resourceKey}/resource:delete")
+    public DeleteResourceResponse deleteResource(@PathVariable("resourceKey") Long resourceKey) {
+         return camundaClient.newDeleteResourceCommand(resourceKey)
                 .send()
                 .join();
     }
