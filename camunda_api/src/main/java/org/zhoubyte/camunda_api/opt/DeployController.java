d74f7514704f2b7ae6a883353f04c77e308eb744
L-zhoujianing@qianxin.com <L-zhoujianing@qianxin.com>
2026-03-25 15:21:36 +0800

    查看部署的流程 和表单信息

--- camunda_api/src/main/java/org/zhoubyte/camunda_api/opt/DeployController.java
+++ camunda_api/src/main/java/org/zhoubyte/camunda_api/opt/DeployController.java
@@ -1,20 +1,33 @@
 package org.zhoubyte.camunda_api.opt;
 
 import io.camunda.client.CamundaClient;
-import io.camunda.client.api.response.DeleteResourceResponse;
 import io.camunda.client.api.response.DeploymentEvent;
+import io.camunda.client.api.search.response.Form;
+import io.camunda.client.api.search.response.ProcessDefinition;
+import io.camunda.client.api.search.response.SearchResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/deploy")
+@Slf4j
 public class DeployController {
 
     private final CamundaClient camundaClient;
 
-    public DeployController(CamundaClient camundaClient) {
+    public DeployController(CamundaClient camundaClient,
+                            @Value("${camunda.client.rest-address}") String restAddress,
+                            @Value("${camunda.client.auth.username}") String username,
+                            @Value("${camunda.client.auth.password}") String password) {
+        log.info("In DeployController, rest-address = {}, username = {}, password = {}", restAddress, username, password);
         this.camundaClient = camundaClient;
     }
 
+    // 部署流程
     @GetMapping(value = "/bpmn_resource")
     public DeploymentEvent deployBpmnResource() {
         return camundaClient.newDeployResourceCommand()
@@ -23,7 +36,7 @@ public class DeployController {
                 .join();
     }
 
-
+    // 部署 Form
     @GetMapping(value = "/form_resource")
     public DeploymentEvent deployFormResource() {
         return camundaClient.newDeployResourceCommand()
@@ -32,11 +45,35 @@ public class DeployController {
                 .join();
     }
 
+    // 调用 API 删除部署的流程会失败，可以使用 Operate UI提供的删除操作
+//    @DeleteMapping(value = "/{resourceKey}/resource:delete")
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public void deleteResource(@PathVariable("resourceKey") Long resourceKey) {
+//        camundaClient.newDeleteResourceCommand(resourceKey)
+//                .send()
+//                .join();
+//    }
 
-    @DeleteMapping(value = "/{resourceKey}/resource:delete")
-    public DeleteResourceResponse deleteResource(@PathVariable("resourceKey") Long resourceKey) {
-         return camundaClient.newDeleteResourceCommand(resourceKey)
-                .send()
-                .join();
+
+    // 查看所有流程和form信息
+    @GetMapping(value = "/mapper")
+    public Map<String, Object> mapper() {
+        Map<String, Object> result = new HashMap<>();
+        SearchResponse<ProcessDefinition> searchResponse = camundaClient.newProcessDefinitionSearchRequest().send().join();
+        searchResponse.items().forEach(item -> {
+            Form form = camundaClient.newProcessDefinitionGetFormRequest(item.getProcessDefinitionKey()).send().join();
+            Map<String, Object> map = new HashMap<>();
+            if(form != null) {
+                if(form.getFormId() != null) {
+                    map.put("formId", form.getFormId());
+                }
+//                map.put("formKey", form.getFormKey());
+            }
+            Map<String, Object> temp = new HashMap<>();
+            temp.put("process", item);
+            temp.put("form", map);
+            result.put(item.getResourceName(), temp);
+        });
+        return result;
     }
 }
